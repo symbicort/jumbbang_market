e6d8cfdc4a4f85474c63a7930333b2789ca7b9a7
jihyehan91 <toto5523@hanmail.net>
2023-12-19 08:57:33 +0900

    main, mypage

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -2,34 +2,34 @@
 
 //메인
 exports.main = (req, res) => {
-	res.render('index');
+  res.render('index');
 };
 
 //로그인
 exports.getLogin = (req, res) => {
-	res.render('login');
+  res.render('login');
 };
 //로그아웃
 exports.postLogout = (req, res) => {
-	res.send('로그아웃 되었습니다'); // 예시
+  res.send('로그아웃 되었습니다'); // 예시
 };
 //회원가입
 exports.getRegister = (req, res) => {
-	res.render('register');
+  res.render('register');
 };
 //마이페이지
 exports.getMypage = (req, res) => {
-	res.render('mypage');
+  res.render('mypage');
 };
 //프로필
 exports.getProfile = (req, res) => {
-	res.render('profile');
+  res.render('profile');
 };
 //통합검색
 exports.getSearch = (req, res) => {
-	res.render('search');
+  res.render('search');
 };
 //고객센터
 exports.getCs = (req, res) => {
-	res.render('cs');
+  res.render('cs');
 };
