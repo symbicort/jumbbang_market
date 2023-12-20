cb68dc01bfe59fae9b37aa765844cfbdd2de4b6f
김상호(KDT web 10) <roo2bos@gmail.com>
2023-12-20 12:54:21 +0900

    채팅 : 프론트 추가

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,9 @@ router.get('/search', controller.getSearch);
 router.get('/cs', controller.getCs);
 router.get('/chats', controller.getChats);
 router.get('/chats/:id', controller.getChats);
+router.get('/chatLogout/:id', controller.getChatLogout);
+router.get('/reviewPost/:id', controller.getReviewPost);
+router.get('/reviewReceive/:id', controller.getReviewReceive);
 
 // module.exports를 통해서 router를 등록해줘야 다른 모듈에서 사용 가능함함.
 module.exports = router;
