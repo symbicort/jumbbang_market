1338cf5838bdcd7a4b6b2765daf2a150434795a4
I00jey <cyj5056@gmail.com>
2023-12-23 09:51:35 +0900

    market product id plus

--- static/js/market.js
+++ static/js/market.js
@@ -72,48 +72,53 @@ function btnBid() {}
 // 중고거래 상세페이지 채팅버튼 함수
 function directChat() {
     const myname = "myname";
-    const productId = "dagfwvvdagafds";
+    const productId = document.getElementById("productid").value;
     const postName = document.querySelector("#nick").innerText;
-    // const postName = "하하";
     window.location.href = `/chatroom?postName=${postName}&myName=${myname}&productId=${productId}`;
 }
 
-async function addpost(){
-	const form = document.forms['marketWrite'];
+async function addpost() {
+    const form = document.forms["marketWrite"];
 
-    if(form.subject.value == '' || form.comment.value == '' || form.priceFirst.value == '' || form.priceDirect.value == '' || form.dateLimit.value == ''){
-        alert('정보를 모두 입력해주세요.');
+    if (
+        form.subject.value == "" ||
+        form.comment.value == "" ||
+        form.priceFirst.value == "" ||
+        form.priceDirect.value == "" ||
+        form.dateLimit.value == ""
+    ) {
+        alert("정보를 모두 입력해주세요.");
         return;
     }
 
-	const image = document.getElementById('image');
+    const image = document.getElementById("image");
 
-	let formData = new FormData();
+    let formData = new FormData();
 
-	formData.append("subject", form.subject.value);
-	formData.append("comment", form.comment.value);
-	formData.append("category", form.category.value)
-	formData.append("state", form.state.value);
-	formData.append("priceFirst", form.priceFirst.value);
-	formData.append("priceDirect", form.priceDirect.value);
-	formData.append("dateLimit", form.dateLimit.value);
+    formData.append("subject", form.subject.value);
+    formData.append("comment", form.comment.value);
+    formData.append("category", form.category.value);
+    formData.append("state", form.state.value);
+    formData.append("priceFirst", form.priceFirst.value);
+    formData.append("priceDirect", form.priceDirect.value);
+    formData.append("dateLimit", form.dateLimit.value);
 
-	for (let i = 0; i < image.files.length; i++) {
-        formData.append('files', image.files[i]);
+    for (let i = 0; i < image.files.length; i++) {
+        formData.append("files", image.files[i]);
     }
 
-	await axios({
-        method: 'POST',
-        url: '/market/write',
+    await axios({
+        method: "POST",
+        url: "/market/write",
         data: formData,
         headers: {
-            'Content-Type': 'multipart/form-data'
-        }
+            "Content-Type": "multipart/form-data",
+        },
     }).then((res) => {
-		console.log(res);
-		if(res){
-			alert('게시물 작성이 완료되었습니다.')
-			document.location.href = '/market'
-		}
-    })
+        console.log(res);
+        if (res) {
+            alert("게시물 작성이 완료되었습니다.");
+            document.location.href = "/market";
+        }
+    });
 }
