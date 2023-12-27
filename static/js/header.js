ff80c7297456dd624c46762030d84994fe2f8dda
김상호(KDT web 10) <roo2bos@gmail.com>
2023-12-27 22:48:37 +0900

    해더 : 햄버거 메뉴 클릭 후 서브 메뉴에서 마우스 아웃시 감추기 기능 추가

--- static/js/header.js
+++ static/js/header.js
@@ -7,108 +7,113 @@
 // });
 
 // 알림아이콘 호버
-document.addEventListener("DOMContentLoaded", function () {
-    const notifyIcon = document.querySelector(".fa-bell");
-    const notify = document.querySelector(".notify");
-    let notifyTimeout;
 
-    notifyIcon.addEventListener("mouseenter", function () {
-        clearTimeout(notifyTimeout);
-        notify.style.display = "block";
-    });
+document.querySelector('.menu-category').addEventListener('mouseleave', function (e) {
+	const categoryMenu = document.querySelector('#categoryMenu');
+	categoryMenu.checked = false;
+});
+document.addEventListener('DOMContentLoaded', function () {
+	const notifyIcon = document.querySelector('.fa-bell');
+	const notify = document.querySelector('.notify');
+	let notifyTimeout;
 
-    notifyIcon.addEventListener("mouseleave", function () {
-        notifyTimeout = setTimeout(function () {
-            notify.style.display = "none";
-        }, 200);
-    });
+	notifyIcon.addEventListener('mouseenter', function () {
+		clearTimeout(notifyTimeout);
+		notify.style.display = 'block';
+	});
 
-    notify.addEventListener("mouseenter", function () {
-        clearTimeout(notifyTimeout);
-    });
+	notifyIcon.addEventListener('mouseleave', function () {
+		notifyTimeout = setTimeout(function () {
+			notify.style.display = 'none';
+		}, 200);
+	});
 
-    notify.addEventListener("mouseleave", function () {
-        notifyTimeout = setTimeout(function () {
-            notify.style.display = "none";
-        }, 200);
-    });
+	notify.addEventListener('mouseenter', function () {
+		clearTimeout(notifyTimeout);
+	});
 
-    // 사용자 아이콘 호버
-    const userIcon = document.querySelector(".fa-user");
-    const userList = document.querySelector(".user-list");
-    let userTimeout;
+	notify.addEventListener('mouseleave', function () {
+		notifyTimeout = setTimeout(function () {
+			notify.style.display = 'none';
+		}, 200);
+	});
 
-    userIcon.addEventListener("mouseenter", function () {
-        clearTimeout(userTimeout);
-        userList.style.display = "block";
-    });
+	// 사용자 아이콘 호버
+	const userIcon = document.querySelector('.fa-user');
+	const userList = document.querySelector('.user-list');
+	let userTimeout;
 
-    userIcon.addEventListener("mouseleave", function () {
-        userTimeout = setTimeout(function () {
-            userList.style.display = "none";
-        }, 200);
-    });
+	userIcon.addEventListener('mouseenter', function () {
+		clearTimeout(userTimeout);
+		userList.style.display = 'block';
+	});
 
-    userList.addEventListener("mouseenter", function () {
-        clearTimeout(userTimeout);
-    });
+	userIcon.addEventListener('mouseleave', function () {
+		userTimeout = setTimeout(function () {
+			userList.style.display = 'none';
+		}, 200);
+	});
 
-    userList.addEventListener("mouseleave", function () {
-        userTimeout = setTimeout(function () {
-            userList.style.display = "none";
-        }, 200);
-    });
+	userList.addEventListener('mouseenter', function () {
+		clearTimeout(userTimeout);
+	});
+
+	userList.addEventListener('mouseleave', function () {
+		userTimeout = setTimeout(function () {
+			userList.style.display = 'none';
+		}, 200);
+	});
 });
 
 // 채팅방 페이지로 이동
 function goChatrooms() {
-    axios({
-        method: "get",
-        url: "/getcurrentuserid",
-    })
-        .then((result) => {
-            const username = result.data.username;
-            console.log(username);
-            // 채팅방 페이지로 이동
-            window.location.href = `/getchatrooms?myName=${username}`;
-        })
-        .catch((err) => {
-            console.log("현재아이디찾기 실패", err);
-        });
+	axios({
+		method: 'get',
+		url: '/getcurrentuserid',
+	})
+		.then((result) => {
+			const username = result.data.username;
+			console.log(username);
+			// 채팅방 페이지로 이동
+			window.location.href = `/getchatrooms?myName=${username}`;
+		})
+		.catch((err) => {
+			console.log('현재아이디찾기 실패', err);
+		});
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    // 이벤트 핸들러 등록 전에 해당 요소가 존재하는지 확인
-    const logoutLink = document.getElementById("logoutLink");
-    if (logoutLink) {
-        document.addEventListener("click", async (event) => {
-            const target = event.target;
-            if (target.id === "logoutLink") {
-                event.preventDefault();
-                const response = await axios.post("/logout");
-                console.log("로그아웃 결과", response.data);
-                window.location.reload(); // 예: 페이지 새로고침
-            }
-        });
-    }
+document.addEventListener('DOMContentLoaded', () => {
+	// 이벤트 핸들러 등록 전에 해당 요소가 존재하는지 확인
+	const logoutLink = document.getElementById('logoutLink');
+	if (logoutLink) {
+		document.addEventListener('click', async (event) => {
+			const target = event.target;
+			if (target.id === 'logoutLink') {
+				event.preventDefault();
+				const response = await axios.post('/logout');
+				console.log('로그아웃 결과', response.data);
+				window.location.reload(); // 예: 페이지 새로고침
+			}
+		});
+	}
 });
 
 // 헤더 작은 검색창
-const headerSearch = document.querySelector(".fa-magnifying-glass");
-const headerSearchInput = document.getElementById("header-search");
+const headerSearch = document.querySelector('.fa-magnifying-glass');
+const headerSearchInput = document.getElementById('header-search');
 
-headerSearch.addEventListener("click", function () {
-    console.log(headerSearchInput);
-    headerSearchInput.classList.toggle("active");
-    headerSearchInput.focus();
+headerSearch.addEventListener('click', function () {
+	console.log(headerSearchInput);
+	headerSearchInput.classList.toggle('active');
+	headerSearchInput.focus();
 });
 
-const searchInputHeader = document.getElementById("header-search");
+const searchInputHeader = document.getElementById('header-search');
 function searchHeader() {
-    if (searchInputHeader.value) {
-        // console.log(searchInputHeader.value);
-        const searchWord = searchInputHeader.value;
-        searchInputHeader.value = "";
-        window.location.href = `/search?searchWord=${searchWord}`;
-    }
+	if (searchInputHeader.value) {
+		// console.log(searchInputHeader.value);
+		const searchWord = searchInputHeader.value;
+		searchInputHeader.value = '';
+		window.location.href = `/search?searchWord=${searchWord}`;
+	}
 }
