ab605c1e25041510a656f926b79c020b893d52b3
jihyehan91 <toto5523@hanmail.net>
2023-12-27 13:04:13 +0900

    메인 미디어쿼리 수정

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,14 @@
 var swiper = new Swiper('.mySwiper', {
-  cssMode: true,
+  effect: 'fade',
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: false,
   pagination: {
     el: '.swiper-pagination',
   },
