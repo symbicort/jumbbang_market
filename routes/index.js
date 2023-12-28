2b8cf8d511eb7d222d86d0bc277a66bc7b07e59f
symbicort <jeongwonlee0@gmail.com>
2023-12-28 19:16:34 +0900

    swagger api 작성

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,242 @@ const controller = require('../controller/Cmain');
 
 //Controller 연결
 //경로를 컨트롤러와 연결지어 사용 가능
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: 메인 페이지 표시
+ *     tags: [Index]
+ *     parameters: []
+//  *     responses:
+//  *       '200':
+//  *         description: 메인 페이지 render, 로그인 된 유저 정보 return
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   get:
+ *     summary: 로그인 페이지 표시
+ *     tags: [Index]
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ * /logout:
+ *   get:
+ *     summary: 현재 접속중인 유저 로그아웃 처리
+ *     tags: [Index]
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ * /register:
+ *   get:
+ *     summary: 회원가입 페이지 표시
+ *     tags: [Index]
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ * /mypage:
+ *   get:
+ *     summary: 마이페이지 표시
+ *     tags: [Index]
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ * /profile:
+ *   get:
+ *     summary: 프로필 페이지 표시
+ *     tags: [Index]
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ * /search:
+ *   get:
+ *     summary: 중고 거래 게시물 검색
+ *     tags: [Index]
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ * /cs:
+ *   get:
+ *     summary: 고객센터 페이지 이동
+ *     tags: [Index]
+ * 
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ *  /reviewReceive/{id}:
+ *   get:
+ *     summary: 리뷰 페이지 표시
+ *     tags: [Index]
+ */
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     tags: [Index]
+ *     summary: 사용자 회원가입 요청
+ *     description: 사용자 회원가입
+ *     requestBody:
+ *       description: 사용자 정보
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userid:
+ *                 type: string
+ *               userpw:
+ *                 type: string
+ *               nickname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               contact:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *             required:
+ *               - userid
+ *               - userpw
+ *               - address
+ */
+
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     tags: [Index]
+ *     summary: 사용자 로그인
+ *     description: 사용자 로그인을 위한 엔드포인트
+ *     requestBody:
+ *       description: 사용자 정보
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userid:
+ *                 type: string
+ *               userpw:
+ *                 type: string
+ */
+
+
+/**
+ * @swagger
+ * /chklogin:
+ *   get:
+ *     summary: 현재 브라우저에 접속한 유저가 로그인 상태인지 확인
+ *     tags: [Index]
+ *     parameters: []
+ */
+
+/**
+ * @swagger
+ * /withdraw:
+ *   post:
+ *     tags: [Index]
+ *     summary: 회원 탈퇴
+ *     description: 현재 접속중인 유저 회원탈퇴
+ *     requestBody:
+ *       description: 접속중인 유저 회원탈퇴
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userid:
+ *                 type: string
+ *               userpw:
+ *                 type: string
+ */
+
+/**
+ * @swagger
+ * /edit_user:
+ *   patch:
+ *     tags: [Index]
+ *     summary: 사용자 정보 수정 요청
+ *     description: 사용자 정보를 수정합니다.
+ *     requestBody:
+ *       description: 수정할 사용자 정보
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userid:
+ *                 type: string
+ *               userpw:
+ *                 type: string
+ *               newpw:
+ *                 type: string
+ *               usernick:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               contact:
+ *                 type: string
+ *               addr:
+ *                 type: string
+ */
+
+/**
+ * @swagger
+ * /edit_userI:
+ *   patch:
+ *     tags: [Index]
+ *     summary: 사용자 정보 수정 요청(프로필 이미지 포함)
+ *     description: 사용자 정보를 수정합니다.(프로필 이미지 포함)
+ *     requestBody:
+ *       description: 수정할 사용자 정보
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userid:
+ *                 type: string
+ *               userpw:
+ *                 type: string
+ *               newpw:
+ *                 type: string
+ *               usernick:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               contact:
+ *                 type: string
+ *               addr:
+ *                 type: string
+ *               image:
+ *                 type: file
+ *       required:
+ *         - userid
+ *         - image
+ */
+
+
 router.get('/', controller.main);
 router.get('/login', controller.getLogin);
 router.get('/logout', controller.getLogout);
--- routes/market.js
+++ routes/market.js
@@ -3,6 +3,179 @@ const router = express.Router();
 
 const controller = require("../controller/Cmarket");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Market
+ *   description: 중고 거래 관련 작업
+ */
+
+/**
+ * @swagger
+ * /market:
+ *   get:
+ *     summary: 전체 중고 거래 게시물 가져오기
+ *     tags: [Market]
+ *     parameters:
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         description: 선택적인 카테고리 코드
+ *     responses:
+ *       '200':
+ *         description: 중고 거래 게시물 목록
+ *         content:
+ *           application/json:
+ *             example:
+ *               postData: []
+ */
+
+/**
+ * @swagger
+ * /marketsort:
+ *   get:
+ *     summary: 중고 거래 게시물 정렬
+ *     tags: [Market]
+ *     parameters:
+ *       - in: query
+ *         name: selectedSort
+ *         schema:
+ *           type: string
+ *         description: 선택된 정렬 옵션
+ *     responses:
+ *       '200':
+ *         description: 정렬된 중고 거래 게시물
+ *         content:
+ *           application/json:
+ *             example:
+ *               postData: []
+ */
+
+/**
+ * @swagger
+ * /articles/{id}:
+ *   get:
+ *     summary: 선택한 중고 거래 게시물 가져오기
+ *     tags: [Market]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: 중고 거래 게시물 ID
+ *     responses:
+ *       '200':
+ *         description: 특정 중고 거래 게시물
+ *         content:
+ *           application/json:
+ *             example:
+ *               postdata: {}
+ */
+
+/**
+ * @swagger
+ * /market/write:
+ *   get:
+ *     summary: 중고 거래 게시물 작성 페이지 표시
+ *     tags: [Market]
+ */
+
+/**
+ * @swagger
+ * /market/write:
+ *   post:
+ *     summary: 중고 거래 게시물 등록
+ *     tags: [Market]
+ *     requestBody:
+ *       description: 중고 거래 게시물 데이터
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             subject: "예시 제목"
+ *             comment: "예시 코멘트"
+ *             category: "예시_카테고리"
+ *             state: 1
+ *             priceFirst: 100
+ *             priceDirect: 150
+ *             dateLimit: "2023-12-31"
+ */
+
+/**
+ * @swagger
+ * /market/bid:
+ *   post:
+ *     summary: 중고 거래 게시물에 입찰하기
+ *     tags: [Market]
+ *     requestBody:
+ *       description: 입찰 데이터
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             bidprice: 120
+ *             productId: "예시_상품_ID"
+ */
+
+/**
+ * @swagger
+ * /market/userchk:
+ *   post:
+ *     summary: 로그인한 사용자가 중고 거래 게시물 소유자인지 확인
+ *     tags: [Market]
+ *     requestBody:
+ *       description: 상품 ID 데이터
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             productId: "예시_상품_ID"
+ *     responses:
+ *       '200':
+ *         description: 사용자가 중고 거래 게시물의 소유자입니다.
+ *         content:
+ *           application/json:
+ *             example:
+ *               islogin: true
+ *               result: true
+ */
+
+/**
+ * @swagger
+ * /market/editArticle:
+ *   patch:
+ *     summary: 중고 거래 게시물 정보 수정
+ *     tags: [Market]
+ *     requestBody:
+ *       description: 업데이트된 중고 거래 게시물 데이터
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             articleid: "예시_게시물_ID"
+ *             subject: "업데이트된 제목"
+ *             content: "업데이트된 내용"
+ *             state: 2
+ */
+
+/**
+ * @swagger
+ * /market/directBuy:
+ *   post:
+ *     summary: 중고 거래 게시물에 직접 구매 요청
+ *     tags: [Market]
+ *     requestBody:
+ *       description: 직접 구매 데이터
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             productId: "예시_상품_ID"
+ */
+
+
 router.get("/market", controller.market);
 router.get("/marketsort", controller.marketsort);
 router.get("/articles/:id", controller.getView);
