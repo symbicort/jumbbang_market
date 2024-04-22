960f34917dd5a620972e62660c51a31f83fd2bee
symbicort <jeongwonlee0@gmail.com>
2024-04-22 14:09:37 +0900

    코드 수정

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,15 +1,15 @@
-const bcrypt = require('bcrypt');
+const { genSaltSync, hashSync, compareSync } = require('bcrypt');
 
 // 1. 비밀번호 해싱 함수
 function hashPW(password) {
-    const salt = bcrypt.genSaltSync(Number(process.env.saltRounds)); // 솔트 생성
-    return bcrypt.hashSync(password, salt); // salt를 자동으로 생성
+    const salt = genSaltSync(Number(process.env.saltRounds)); // 솔트 생성
+    return hashSync(password, salt); // salt를 자동으로 생성
 }
 
 // 2. 원본 비밀번호와 해시된 비밀번호가 일치하는지 확인하는 함수(같은지, 다른지만 알려줌)
 function comparePW(password, hashedPW){
     console.log(hashedPW);
-    return bcrypt.compareSync(password, hashedPW)
+    return compareSync(password, hashedPW)
 }
 
 
